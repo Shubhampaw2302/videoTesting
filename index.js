f0a5216e7fb241752548530259a71bfdd62da503
Shubhampaw2302 <71928018+Shubhampaw2302@users.noreply.github.com>
2022-01-20 17:52:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-var video = document.querySelector("#videoElement");
+navigator.mediaDevices.enumerateDevices().then((devices) => {
+    let videoSourcesSelect = document.getElementById("video-source");
 
-if (navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia({ video: true })
-    .then(function (stream) {
-      video.srcObject = stream;
-    })
-    .catch(function (err0r) {
-      console.log("Something went wrong!");
+    // Iterate over all the list of devices (InputDeviceInfo and MediaDeviceInfo)
+    devices.forEach((device) => {
+        let option = new Option();
+        option.value = device.deviceId;
+
+        // According to the type of media device
+        switch(device.kind){
+            // Append device to list of Cameras
+            case "videoinput":
+                option.text = device.label || `Camera ${videoSourcesSelect.length + 1}`;
+                videoSourcesSelect.appendChild(option);
+                console.log(option.text);
+                break;
+        }
+
+        var video = document.querySelector("#videoElement");
+
+        if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(function (stream) {
+            video.srcObject = stream;
+            })
+            .catch(function (err0r) {
+            console.log("Something went wrong!");
+            });
+        }
+
+        
     });
-}
+}).catch(function (e) {
+    console.log(e.name + ": " + e.message);
+});
